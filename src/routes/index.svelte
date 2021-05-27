0cdbc34071386602586a57e69c0e6bde84661055
Max Starr <manmon42@gmail.com>
2021-05-27 01:27:44 -0700

    init layout

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+	<title>Max Starr</title>
+</svelte:head>
+
+<div class="container">
+	<div class="card-container">
+		<main class="card">
+			<h1>Hi, I'm Max</h1>
+			<p>I'm a web engineer</p>
+		</main>
+	</div>
+</div>
+
+<style>
+	.container {
+		display: grid;
+		grid-template-columns: repeat(5, 1fr);
+		height: 100%;
+	}
+
+	.card-container {
+		grid-column: 2;
+		display: flex;
+		align-items: center;
+	}
+	.card {
+		background-color: var(--light);
+		color: var(--dark);
+		padding: 1em;
+		width: max-content;
+	}
+
+	h1 {
+		color: var(--text-dark);
+	}
+</style>
