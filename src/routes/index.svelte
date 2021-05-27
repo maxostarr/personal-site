c12681891b158178efe09eb07d0572783350d282
Max Starr <manmon42@gmail.com>
2021-05-27 00:31:10 -0700

    favicon

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,32 @@
 <script>
+  import { fly } from "svelte/transition"
+  import { expoOut } from "svelte/easing"
+  import { onMount } from "svelte"
+
+  let ready = false
+  onMount(() => setTimeout(() => (ready = true), 400))
 </script>
 
 <svelte:head>
   <title>Max Starr</title>
 </svelte:head>
 
-<div class="container">
-  <div>
-    <h1>Hi,</h1>
-    <h2>I'm Max</h2>
-    <p>I make websites</p>
+{#if ready}
+  <div class="container">
+    <div
+      in:fly={{
+        x: 100,
+        duration: 2000,
+        easing: expoOut,
+      }}
+      out:fly
+    >
+      <h1>Hi,</h1>
+      <h2>I'm Max</h2>
+      <p>I make websites</p>
+    </div>
   </div>
-</div>
+{/if}
 
 <style>
   .container {
