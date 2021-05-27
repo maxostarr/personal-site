b4502b92bcd2a404f86e5b5ab3387f9f706138d6
Max Starr <manmon42@gmail.com>
2021-05-27 01:38:46 -0700

    initial layout

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,46 @@
+<script>
+	import { fly } from 'svelte/transition';
+	import { expoOut } from 'svelte/easing';
+	import { onMount } from 'svelte';
+	let ready = false;
+	onMount(() => setTimeout(() => (ready = true), 400));
+</script>
+
 <svelte:head>
 	<title>Max Starr</title>
 </svelte:head>
 
 <div class="container">
-	<div class="card-container">
-		<main class="card">
-			<h1>Hi, I'm Max</h1>
-			<p>I'm a web engineer</p>
-		</main>
-	</div>
+	{#if ready}
+		<div class="grid-item col-2">
+			<main
+				class="card"
+				in:fly={{
+					x: 100,
+					duration: 2000,
+					easing: expoOut
+				}}
+				out:fly
+			>
+				<h1>Hi, I'm Max</h1>
+				<p>I'm a web engineer</p>
+			</main>
+		</div>
+		<div class="grid-item col-3">
+			<div
+				class="card"
+				in:fly={{
+					delay: 100,
+					y: -100,
+					duration: 2000,
+					easing: expoOut
+				}}
+				out:fly
+			>
+				<img src="/max.png" alt="Max Starr" />
+			</div>
+		</div>
+	{/if}
 </div>
 
 <style>
@@ -18,11 +50,17 @@
 		height: 100%;
 	}
 
-	.card-container {
-		grid-column: 2;
+	.grid-item {
 		display: flex;
 		align-items: center;
 	}
+	.col-2 {
+		grid-column: 2;
+	}
+	.col-3 {
+		grid-column: 3;
+	}
+
 	.card {
 		background-color: var(--light);
 		color: var(--dark);
