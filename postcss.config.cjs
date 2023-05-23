8956b3f7433a77397dea56e7f17cbc1ae6e9e9db
Max Starr <max@maxstarr.xyz>
2023-05-23 17:59:21 -0500

    add postcss

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,5 +1,14 @@
+const postcssPresetEnv = require('postcss-preset-env');
+
 module.exports = {
   plugins: [
+    postcssPresetEnv({
+      features: {
+        'nesting-rules': true,
+        // Enable custom selector 
+        'custom-selectors': true,
+      }
+    }),
     require('autoprefixer'),
     require('cssnano'),
   ],
